cba4594ee372d4f9e8790ecfd42a388352c08fae
krishna <singhkrishk07@gmail.com>
2020-02-24 21:37:01 +0530

    some fixes

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(redisClient);
 
 app.use(express.static(path.join(__dirname,  "client/build")));
 app.get('*', function(req, res) {
-	res.sendFile(path.resolve(__dirname, 'client/build'));
+	res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 });
 
 app.use('/api', userRouter);
